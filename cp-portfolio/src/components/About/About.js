8bdfb489fa10b2098abadd1de2e00d05a937433f
Poonkodi Chenniappan <68348916+cpoonkodi@users.noreply.github.com>
2025-01-20 00:55:29 +0200

    Update About.js

--- cp-portfolio/src/components/About/About.js
+++ cp-portfolio/src/components/About/About.js
@@ -9,7 +9,7 @@ function About() {
           <div class="content">
             <h1>ABOUT ME</h1>
             <p>
-              Enthustatic Frontend developer, re-kick started my IT career recently. Interested in Animations. Currently getting training from Integrify, Helsinki.
+              Enthustatic Frontend developer, re-kick started my IT career recently. Interested in Web development. Currently getting training from Hive, Helsinki.
             </p>
           </div>
         </div>
