0c666a4b9b439e0a1a107bb9d7724a4572e605bc
Poonkodi Chenniappan <68348916+cpoonkodi@users.noreply.github.com>
2025-01-20 00:04:49 +0200

    Update Skills.js

--- cp-portfolio/src/components/Skills/Skills.js
+++ cp-portfolio/src/components/Skills/Skills.js
@@ -1,6 +1,7 @@
 import "./Skills.css";
 import { ProgressBar } from "react-bootstrap";
 
+import c from "../../assets/c.png";
 import cpp from "../../assets/c++.jpeg";
 import html from "../../assets/html.png";
 import css from "../../assets/css.jpeg";
@@ -28,26 +29,18 @@ function Skills() {
 
         <div class="container">
           <div class="row p-lg-3 p-0 mb-lg-3 mb-0">
-            <div class="col-lg-6 col-12 pt-lg-0 pt-3">
-              <img src={js} alt="Javascript logo" />
-            </div>
-            <div class="col-lg-6 col-12 pt-lg-0 pt-3">
+            <div class="col-lg-4 col-12 pt-lg-0 pt-3">
               <img src={react} alt="React logo" />
+            </div>
+            <div class="col-lg-4 col-12 pt-lg-0 pt-3">
+              <img src={c} alt="C logo" />
+            </div>
+            <div class="col-lg-4 col-12 pt-lg-0 pt-3">
+              <img src={cpp} alt="C++ logo" />
             </div>
           </div>
         </div>
 
-        <div class="container">
-          <div class="row p-lg-3 p-0 mb-lg-3 mb-0">
-            <div class="col-lg-6 col-12 pt-lg-0 pt-3">
-            <img src={cpp} alt="C++ logo" />
-            </div>
-            <div class="col-lg-6 col-12 pt-lg-0 pt-3">
-              <label>Animation</label>
-              <ProgressBar now={60} label={`${60}%`} />
-            </div>
-          </div>
-        </div>
       </section>
     </div>
   );
