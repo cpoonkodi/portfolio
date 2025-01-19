4db1096fce35945272143d6f9672223739bd626b
Poonkodi Chenniappan <68348916+cpoonkodi@users.noreply.github.com>
2025-01-20 00:54:20 +0200

    Update Home.js

--- cp-portfolio/src/components/Home/Home.js
+++ cp-portfolio/src/components/Home/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <section id="home">
         <h1 class="text-center">Poonkodi Chenniappan </h1>
-        <p class="text-center">Junior Developer</p>
+        <p class="text-center">Web Developer</p>
       </section>
     </div>
   );
