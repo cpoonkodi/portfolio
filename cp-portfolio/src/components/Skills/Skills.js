b861d77052e6517adeef45081eb25a9f7da88b7f
Poonkodi Chenniappan <68348916+cpoonkodi@users.noreply.github.com>
2025-01-19 23:32:22 +0200

    Update Skills.js

--- cp-portfolio/src/components/Skills/Skills.js
+++ cp-portfolio/src/components/Skills/Skills.js
@@ -10,12 +10,12 @@ function Skills() {
           <div class="row p-lg-3 p-0 mb-lg-3 mb-0">
             <div class="col-lg-6 col-12 pt-lg-0 pt-3">
               <li> HTML </li>
-              <label>HTML</label>
-              <ProgressBar now={80} label={`${80}%`} />
+              <label> -- HTML</label>
+              //<ProgressBar now={80} label={`${80}%`} />
             </div>
             <div class="col-lg-6 col-12 pt-lg-0 pt-3">
               <label>CSS</label>
-              <ProgressBar now={70} label={`${70}%`} />
+              //<ProgressBar now={70} label={`${70}%`} />
             </div>
           </div>
         </div>
@@ -24,11 +24,11 @@ function Skills() {
           <div class="row p-lg-3 p-0 mb-lg-3 mb-0">
             <div class="col-lg-6 col-12 pt-lg-0 pt-3">
               <label>Javascript</label>
-              <ProgressBar now={60} label={`${60}%`} />
+              //<ProgressBar now={60} label={`${60}%`} />
             </div>
             <div class="col-lg-6 col-12 pt-lg-0 pt-3">
               <label>React</label>
-              <ProgressBar now={50} label={`${50}%`} />
+              //<ProgressBar now={50} label={`${50}%`} />
             </div>
           </div>
         </div>
